c6a88e7a97b3f171cf4ed6a9f6b3d5f7b2991e90
irvinzato <multizato@hotmail.com>
2022-05-17 14:07:30 -0500

    Instalacion de nodemon global y modificacion de package.json para ambiente dev y produccion, equivalente a start

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Hola desde node");
+console.log("Hola desde node, creacion de back");
+console.log("Instalacion de nodemon! para que recarge solo, usando el comando -nodemon index.js-");
+console.log("Para utilizar las variables del package.json hay que utilizar -npm run dev- para el caso de desarrollo");
