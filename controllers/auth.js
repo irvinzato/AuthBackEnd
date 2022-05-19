e3c8dddd4f52544280d4684829255c5dbc174900
irvinzato <multizato@hotmail.com>
2022-05-18 22:32:26 -0500

    Validaciones en campos que recibo realizadas con uso de check y mostrando en caso que halla errores en algun campo desde el controlador

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
 const { response } = require('express');    //Exportacion para tener todos los metodos posibles en "res", por eso lo igualo a response
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
-    console.log( req.body );
+   
+    const errors = validationResult( req );
+    console.log(errors);
+
+    if( !errors.isEmpty() ){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
     const { name, email, password } = req.body;    //Puedo desestructurar los datos que me llegan
     console.log("Recibi ", name, email, password );
     return res.json({
@@ -10,7 +21,18 @@ const crearUsuario = (req, res = response) => {
     });
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+
+    const errors = validationResult( req );
+    console.log(errors);
+
+    if( !errors.isEmpty() ){    //Si no esta vacio mi arraglo de errores
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
     const { email, password } = req.body;
     console.log("Recibi ", email, password);
     return res.json({
@@ -19,7 +41,7 @@ const loginUsuario = (req, res) => {
     });
 }
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
     return res.json({
         ok: true,
         msg: 'Renew token /renew'
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,24 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 const router = Router();
 
 //Crear un nuevo usuario
 //Como abra mas logica en el callBack se crea en controllers "crearUsuario" y se exporta aqui su funcion
-router.post( '/new', crearUsuario );    
+router.post( '/new', [
+    check('name', 'El nombre es obligatorio y debe tener minimo 3 caracteres' ).not().isEmpty().isLength({ min: 3 }),
+    check('email', 'El email es obligatorio' ).isEmail(),
+    check('password', 'La contraseña es obligatoria y requiere 6 caracteres minimo' ).isLength({ min: 6 })
+] , crearUsuario );    
 
 //Login de usuario
-router.post( '/', loginUsuario );
+//Añadire middlewares para que mi servicio solo se ejecute si recibe los campos que son necesarios, puede ser mas de 1
+//Primero ejecuta los middlewares y despues el controlador, cuando se pasa por el middlewares ahora tenemos un nuevo objeto que podemos usar en el controlador
+router.post( '/', [
+    check('email', 'El email es obligatorio' ).isEmail(),
+    check('password', 'La contraseña es obligatoria y requiere 6 caracteres minimo' ).isLength({ min: 6 })
+] ,loginUsuario );
 
 //Validar y revalidar token
 router.get( '/renew', revalidarToken );
